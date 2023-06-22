ec6dedab880bdbef91c3a3b1b139fed57c7581f6
Rohith Reddy <yrohithreddy12@gmail.com>
2023-06-22 19:49:35 +0530

    edited title in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,12 +59,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | RohithReddy";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            $("#favicon").attr("href", "assets/images/favicon.png");
         }
     });
 
