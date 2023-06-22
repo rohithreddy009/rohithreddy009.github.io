09aa95597c592a8c109e920d371f53c11c5a6dc7
Rohith Reddy <yrohithreddy12@gmail.com>
2023-06-22 23:49:36 +0530

    fixed header bug in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,11 +60,11 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | RohithReddy";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/favicon.ico");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/favicon.ico");
         }
     });
 
