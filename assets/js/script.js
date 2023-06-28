98a8679fa34b6ca182e7841374f1b58424eb313a
Rohith Reddy <yrohithreddy12@gmail.com>
2023-06-28 12:37:04 +0530

    submit button is working now

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "Fullstack Development"],
+    strings: ["frontend development", "backend development", "Web Design",],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
