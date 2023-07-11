faf09331cd3fedee65a7cd7cd9bd343bd8a80775
Rohith Reddy <yrohithreddy12@gmail.com>
2023-07-11 14:23:23 +0530

    tawk to initialized

--- experience/script.js
+++ experience/script.js
@@ -30,18 +30,17 @@ srtop.reveal('.experience .timeline', { delay: 400 });
 srtop.reveal('.experience .timeline .container', { interval: 400 });
 
 
-// Start of Tawk.to Live Chat
+
+
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/64ad0f9894cf5d49dc62d434/1h5209rdc';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
 })();
-// End of Tawk.to Live Chat
-
 
 // disable developer mode
 document.onkeydown = function (e) {
--- projects/script.js
+++ projects/script.js
@@ -105,17 +105,22 @@ getProjects().then(data => {
 })
 // fetch projects end
 
-// Start of Tawk.to Live Chat
+
+//
+// <!--Start of Tawk.to Script-->
+// {/* <script type="text/javascript"> */ }
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/64ad0f9894cf5d49dc62d434/1h5209rdc';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
 })();
-// End of Tawk.to Live Chat
+// {/* </script> */ }
+// <!--End of Tawk.to Script-->
+//
 
 // disable developer mode
 document.onkeydown = function (e) {
